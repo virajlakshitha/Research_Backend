f9f82300bb8cbcb2af8afcc498692d2045e062d3
virajlakshitha <virajlakshitha39@gmail.com>
2019-10-31 13:06:56 +0530

    Modified

--- src/main/java/com/tech/ring/support/scrape.java
+++ src/main/java/com/tech/ring/support/scrape.java
@@ -1,38 +1,33 @@
 package com.tech.ring.support;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tech.ring.service.SmtpMail;
-
 @RestController
 public class scrape {
-	
-	@Autowired
-	private SmtpMail smtpMail;
 	
 	@Scheduled(initialDelay = 1000, fixedRate = 86400000)
 	public void ram_scrape() {
 	    try {
 	    	System.out.println("Schedule running");
+	    	//ram
 	    	String Path = "python C:\\Users\\viraj\\Desktop\\Research_BackEnd\\Python\\Search_products_v1.py";
 	    	Process p = Runtime.getRuntime().exec(Path);
-//	    	notifyPrice("virajlkgunathilaka96@gmail.com", "RAM", "14500.00");
+	    	//vga
+//	    	String Path_vga = "python C:\\Users\\viraj\\Desktop\\Research_BackEnd\\Python\\Search_products_v1.py";
+//	    	Process p_vga = Runtime.getRuntime().exec(Path_vga);
+	    	//cpu
+//	    	String Path_cpu = "python C:\\Users\\viraj\\Desktop\\Research_BackEnd\\Python\\Search_products_v1.py";
+//	    	Process p_cpu = Runtime.getRuntime().exec(Path_cpu);
+	    	//motherboard
+//	    	String Path_motherboard = "python C:\\Users\\viraj\\Desktop\\Research_BackEnd\\Python\\Search_products_v1.py";
+//	    	Process p_motherboard = Runtime.getRuntime().exec(Path_motherboard);
+	    	//hard_disk
+//	    	String Path_hard = "python C:\\Users\\viraj\\Desktop\\Research_BackEnd\\Python\\Search_products_v1.py";
+//	    	Process p_hard = Runtime.getRuntime().exec(Path_hard);
 	    } catch(Exception e) {
 	    	e.printStackTrace();
 	    }
-	}
-	
-	public void notifyPrice(String to, String product, String price) {
-		
-		try {
-			smtpMail.send(to, "Price of "+product+" is down to "+price);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
 	}
 
 }
